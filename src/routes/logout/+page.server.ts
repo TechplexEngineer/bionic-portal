4754a7c4951662eba2ae3cfee296ab1d7eaf3e32
Blake Bourque <techplex.engineer@gmail.com>
2026-09-07 10:47:12 -0400

    Fix logout cross-site form error

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,13 +1,17 @@
 import { redirect } from "@sveltejs/kit";
 import * as auth from "$lib/server/auth";
-import type { Actions } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
+
+async function logout(event: Parameters<NonNullable<Actions["default"]>>[0]) {
+	if (event.locals.session && event.platform) {
+		await auth.invalidateSession(event.locals.session.id, event.platform);
+	}
+	auth.deleteSessionTokenCookie(event);
+	redirect(303, "/login");
+}
+
+export const load: PageServerLoad = logout;
 
 export const actions: Actions = {
-	default: async (event) => {
-		if (event.locals.session && event.platform) {
-			await auth.invalidateSession(event.locals.session.id, event.platform);
-		}
-		auth.deleteSessionTokenCookie(event);
-		redirect(303, "/login");
-	}
+	default: logout
 };
